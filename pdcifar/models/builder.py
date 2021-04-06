2cadb48f4500ab05cf5ab509a4a5d61f8d5afe86
itisianlee <ianleedream@163.com>
2021-04-06 21:29:05 +0800

    adapt new config

--- pdcifar/models/builder.py
+++ pdcifar/models/builder.py
@@ -2,8 +2,8 @@ from ..utils import Registry
 
 classifier = Registry('classifier')
 
-def build_classifier(name):
-    if name not in classifier:
-        raise KeyError(f'{name} is not in the {classifier.name} registry')
-    net = classifier.get(name)
+def build_classifier(cfg):
+    if cfg.name not in classifier:
+        raise KeyError(f'{cfg.name} is not in the {classifier.name} registry')
+    net = classifier.get(cfg.name)
     return net()
