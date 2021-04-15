21b80165e7413dce85c0e3c48f8d552e8a08fcee
itisianlee <ianleedream@163.com>
2021-04-15 21:30:17 +0800

    add shufflenet series

--- pdcifar/models/__init__.py
+++ pdcifar/models/__init__.py
@@ -3,3 +3,5 @@ from .transformer import *
 from .mobilenetv1 import mobilenetv1
 from .mobilenetv2 import mobilenetv2
 from .vgg import *
+from .shufflenetv1 import shufflenetv1
+from .shufflenetv2 import shufflenetv2
