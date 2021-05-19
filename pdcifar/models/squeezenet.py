f76fadd435de34b5151500252e0049e46d2777aa
itisianlee <ianleedream@163.com>
2021-05-19 22:13:23 +0800

    fix bug

--- pdcifar/models/squeezenet.py
+++ pdcifar/models/squeezenet.py
@@ -2,7 +2,7 @@ import paddle
 import paddle.nn as nn
 
 
-class Fire(nn.Module):
+class Fire(nn.Layer):
 
     def __init__(
         self,
@@ -13,24 +13,21 @@ class Fire(nn.Module):
     ):
         super(Fire, self).__init__()
         self.inplanes = inplanes
-        self.squeeze = nn.Conv2d(inplanes, squeeze_planes, kernel_size=1)
-        self.squeeze_activation = nn.ReLU(inplace=True)
-        self.expand1x1 = nn.Conv2d(squeeze_planes, expand1x1_planes,
-                                   kernel_size=1)
-        self.expand1x1_activation = nn.ReLU(inplace=True)
-        self.expand3x3 = nn.Conv2d(squeeze_planes, expand3x3_planes,
-                                   kernel_size=3, padding=1)
-        self.expand3x3_activation = nn.ReLU(inplace=True)
+        self.squeeze = nn.Conv2D(inplanes, squeeze_planes, kernel_size=1)
+        self.squeeze_activation = nn.ReLU()
+        self.expand1x1 = nn.Conv2D(squeeze_planes, expand1x1_planes, kernel_size=1)
+        self.expand1x1_activation = nn.ReLU()
+        self.expand3x3 = nn.Conv2D(squeeze_planes, expand3x3_planes, kernel_size=3, padding=1)
+        self.expand3x3_activation = nn.ReLU()
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x):
         x = self.squeeze_activation(self.squeeze(x))
-        return torch.cat([
+        return paddle.concat([
             self.expand1x1_activation(self.expand1x1(x)),
-            self.expand3x3_activation(self.expand3x3(x))
-        ], 1)
+            self.expand3x3_activation(self.expand3x3(x))], 1)
 
 
-class SqueezeNet(nn.Module):
+class SqueezeNet(nn.Layer):
 
     def __init__(
         self,
@@ -41,31 +38,31 @@ class SqueezeNet(nn.Module):
         self.num_classes = num_classes
         if version == '1_0':
             self.features = nn.Sequential(
-                nn.Conv2d(3, 96, kernel_size=7, stride=2),
-                nn.ReLU(inplace=True),
-                nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+                nn.Conv2D(3, 96, kernel_size=7, stride=2),
+                nn.ReLU(),
+                nn.MaxPool2D(kernel_size=3, stride=2, ceil_mode=True),
                 Fire(96, 16, 64, 64),
                 Fire(128, 16, 64, 64),
                 Fire(128, 32, 128, 128),
-                nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+                nn.MaxPool2D(kernel_size=3, stride=2, ceil_mode=True),
                 Fire(256, 32, 128, 128),
                 Fire(256, 48, 192, 192),
                 Fire(384, 48, 192, 192),
                 Fire(384, 64, 256, 256),
-                nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+                nn.MaxPool2D(kernel_size=3, stride=2, ceil_mode=True),
                 Fire(512, 64, 256, 256),
             )
         elif version == '1_1':
             self.features = nn.Sequential(
-                nn.Conv2d(3, 64, kernel_size=3, stride=2),
-                nn.ReLU(inplace=True),
-                nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+                nn.Conv2D(3, 64, kernel_size=3, stride=2),
+                nn.ReLU(),
+                nn.MaxPool2D(kernel_size=3, stride=2, ceil_mode=True),
                 Fire(64, 16, 64, 64),
                 Fire(128, 16, 64, 64),
-                nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+                nn.MaxPool2D(kernel_size=3, stride=2, ceil_mode=True),
                 Fire(128, 32, 128, 128),
                 Fire(256, 32, 128, 128),
-                nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+                nn.MaxPool2D(kernel_size=3, stride=2, ceil_mode=True),
                 Fire(256, 48, 192, 192),
                 Fire(384, 48, 192, 192),
                 Fire(384, 64, 256, 256),
@@ -79,27 +76,18 @@ class SqueezeNet(nn.Module):
                              "1_0 or 1_1 expected".format(version=version))
 
         # Final convolution is initialized differently from the rest
-        final_conv = nn.Conv2d(512, self.num_classes, kernel_size=1)
+        final_conv = nn.Conv2D(512, self.num_classes, kernel_size=1)
         self.classifier = nn.Sequential(
             nn.Dropout(p=0.5),
             final_conv,
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.AdaptiveAvgPool2d((1, 1))
         )
 
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                if m is final_conv:
-                    init.normal_(m.weight, mean=0.0, std=0.01)
-                else:
-                    init.kaiming_uniform_(m.weight)
-                if m.bias is not None:
-                    init.constant_(m.bias, 0)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x):
         x = self.features(x)
         x = self.classifier(x)
-        return torch.flatten(x, 1)
+        return paddle.flatten(x, 1)
 
 
 def _squeezenet(version, **kwargs):
@@ -107,7 +95,7 @@ def _squeezenet(version, **kwargs):
     return model
 
 
-def squeezenet1_0(pretrained=False, progress=True, **kwargs):
+def squeezenet1_0(**kwargs):
     r"""SqueezeNet model architecture from the `"SqueezeNet: AlexNet-level
     accuracy with 50x fewer parameters and <0.5MB model size"
     <https://arxiv.org/abs/1602.07360>`_ paper.
@@ -116,10 +104,10 @@ def squeezenet1_0(pretrained=False, progress=True, **kwargs):
         pretrained (bool): If True, returns a model pre-trained on ImageNet
         progress (bool): If True, displays a progress bar of the download to stderr
     """
-    return _squeezenet('1_0', pretrained, progress, **kwargs)
+    return _squeezenet('1_0', **kwargs)
 
 
-def squeezenet1_1(pretrained=False, progress=True, **kwargs):
+def squeezenet1_1(**kwargs):
     r"""SqueezeNet 1.1 model from the `official SqueezeNet repo
     <https://github.com/DeepScale/SqueezeNet/tree/master/SqueezeNet_v1.1>`_.
     SqueezeNet 1.1 has 2.4x less computation and slightly fewer parameters
@@ -129,4 +117,4 @@ def squeezenet1_1(pretrained=False, progress=True, **kwargs):
         pretrained (bool): If True, returns a model pre-trained on ImageNet
         progress (bool): If True, displays a progress bar of the download to stderr
     """
-    return _squeezenet('1_1', pretrained, progress, **kwargs)
+    return _squeezenet('1_1', **kwargs)
